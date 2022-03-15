356dd2797e96d8438fd400847d844d4a5aea423e
Mateusz Pogrebniak <93048478+ShadowRaven14@users.noreply.github.com>
2022-03-15 17:14:28 +0100

    Add files via upload

---
+++ SPOJ - łatwe - Obżartuchy/SPOJ - łatwe - Obżartuchy/SPOJ - łatwe - Obżartuchy.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+
+
+int main()
+{
+    int doba = 86400;
+
+    int lg = 1, pudelka, n = 0, a = 0;
+    //std::cin >> lg;
+    std::cin >> pudelka;
+    std::vector <int> sekundy(lg);
+    std::cin >> sekundy[0];
+
+    /*
+    for (int i = 0; i < lg; i++)
+    {
+        std::cin >> sekundy[i];
+    }
+    */
+
+    while (doba >= sekundy[0])
+    {
+        doba = doba - sekundy[0];
+        n++;
+    }
+    std::cout << n << " ";
+
+    while (n >= pudelka)
+    {
+        n = n - pudelka;
+        a++;
+    }
+
+    if (n > 0) a++;
+
+    std::cout << a;
+
+    return 0;
+}
---
+++ SPOJ - średnie - Ciekawa wyliczanka/SPOJ - średnie - Ciekawa wyliczanka/SPOJ - średnie - Ciekawa wyliczanka.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+void strange(int n, std::vector<int>& liczba)
+{
+
+    if (liczba[n] == 5)
+    {
+        liczba[n] = 6;
+    }
+    else if (liczba[n] == 6)
+    {
+        liczba[n] = 5;
+        if (n > 0) strange(n - 1, liczba);
+        if (n == 0) liczba.push_back(5);
+    }
+}
+
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    int const p = 5, s = 6;
+    std::vector <int> liczba;
+
+
+    if (n > 0) liczba.push_back(5);
+
+    for (int i = 1; i < n; i++)
+    {
+        strange(liczba.size() - 1, liczba);
+    }
+
+    std::cout << std::endl;
+    for (int i = 0; i < liczba.size(); i++)
+    {
+        std::cout << liczba[i];
+    }
+
+    return 0;
+}
